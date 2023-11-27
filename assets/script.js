51efdf08377d7802e0673660c7c50c8d8fcdb783
sari-rahul <>
2023-11-27 20:20:16 +0000

    Add script.js file and add google maps to interest page

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-        });
-}
